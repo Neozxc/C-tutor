9324eac1b8cf55ae26ead2ce36a036f34a1b5e19
Nedas <neduska991@gmail.com>
2022-05-20 23:01:54 +0100

    .

---
+++ helloworld.c
@@ -0,0 +1,46 @@
+// Uncomment to run some tests. I will track here my progress on C language.
+
+
+// ----------------------------------------------------------------------------------------- //PART #1
+// #include <stdio.h>
+
+// // Starting from the top working down to the bottom
+// int main() {
+
+//     printf("I like pizza\n");
+//     printf("It's really good\n");
+
+//     // We return 0 if our program runs successfully with no errors
+//     // If there is error then we will return 1
+//     // We need return 0 for now to check for any errors
+//     return 0;
+// }
+
+// ----------------------------------------------------------------------------------------- //PART #2
+// #include <stdio.h>
+
+// int main() {
+
+//     // Basic data types
+//     int x; // declaration
+//     x = 123; // initialization
+//     int y = 321; // declaration & intiliziation
+
+//     int age = 21; // intiger
+//     float gpa = 2.05; // floating point number, its a number that contains decimal
+//     char grade = 'C'; // single character
+//     char name[] = "Bro"; // array of character
+
+
+//     // Format specifiers
+//     // %s for strings
+//     // %d for integers
+//     // %c for single character
+//     // %f for floats
+
+//     printf("Your name is %s and you are %d years old, your average grade is %c, your gpa is %f", name, age, grade, gpa);
+
+//     return 0;
+// }
+
+// ----------------------------------------------------------------------------------------- //PART #3
