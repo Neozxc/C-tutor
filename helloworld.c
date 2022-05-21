c8fab771bab00a293fcdfe86279e1e704ceb7b41
Nedas <neduska991@gmail.com>
2022-05-21 16:49:56 +0100

    .

--- helloworld.c
+++ helloworld.c
@@ -146,6 +146,19 @@
 
 // int main() {
 
+//     const double PI = 3.14159;
+//     double radius;
+//     double circumference;
+//     double area;
+
+//     printf("Enter radius of a circle: ");
+//     scanf("%lf", &radius);
+
+//     circumference = 2 * PI * radius;
+//     area = PI * radius * radius;
+//     printf("\ncircumference: %lfm", circumference);
+//     printf("\narea: %lfm", area);
+
 
 //     return 0;
 // }
