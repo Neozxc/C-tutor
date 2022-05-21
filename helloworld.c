f48231001031841cdad61e827f5158ba9afa078b
Nedas <neduska991@gmail.com>
2022-05-21 17:03:44 +0100

    .

--- helloworld.c
+++ helloworld.c
@@ -212,9 +212,34 @@
 
 // int main() {
 
+//     char grade;
+
+//     printf("\nEnter a letter grade: ");
+//     scanf("%c", &grade);
+
+//     switch (grade) {
+//         case 'A':
+//             printf("Perfect!\n");
+//             break;
+//         case 'B':
+//             printf("You did good!\n");
+//             break;
+//         case 'C':
+//             printf("You did okay!\n");
+//             break;
+//         case 'D': 
+//             printf("At least it's not an F!\n");
+//             break;
+//         case 'F':
+//             printf("You failed!\n");
+//             break;
+//         default: 
+//             printf("Please enter a letter grade!");
+//     }
 
 //     return 0;
 // }
+
 // ----------------------------------------------------------------------------------------- //PART #11
 
 // #include <stdio.h>
