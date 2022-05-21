21d96cc852e0a167c121862d58e1f18f89bb3590
Nedas <neduska991@gmail.com>
2022-05-21 21:22:26 +0100

    Calculator

--- helloworld.c
+++ helloworld.c
@@ -278,6 +278,40 @@
 
 // int main() {
 
+//     char operator;
+//     double num1;
+//     double num2;
+//     double result;
+
+//     printf("\nEnter an operator (+ - * /): ");
+//     scanf("%c", &operator);
+//     printf("Enter number 1: ");
+//     scanf("%lf", &num1);
+//     printf("Enter number 2: ");
+//     scanf("%lf", &num2);
+
+//     switch (operator) {
+//         case '+':
+//             result = num1 + num2;
+//             printf("\nResult: %.0lf", result);
+//             break;
+//         case '-':
+//             result = num1 - num2;
+//             printf("\nResult: %.0lf", result);
+//             break;
+//         case '*':
+//             result = num1 * num2;
+//             printf("\nResult: %.0lf", result);
+//             break;
+//         case '/':
+//             result = num1 / num2;
+//             printf("\nResult: %.0lf", result);
+//             break;   
+
+//         default: 
+//             printf("%c is not valid.");
+//     }
+
 
 //     return 0;
 // }
