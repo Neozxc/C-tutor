0ff0a84b47263dde5118b0a69f9a8a30c32eeff6
Nedas <neduska991@gmail.com>
2022-05-21 16:53:46 +0100

    .

--- helloworld.c
+++ helloworld.c
@@ -165,8 +165,22 @@
 
 // ----------------------------------------------------------------------------------------- //PART #8
 // #include <stdio.h>
+// #include <math.h>
 
 // int main() {
+
+//     double A;
+//     double B;
+//     double C;
+
+//     printf("Enter side A: ");
+//     scanf("%lf", &A);
+//     printf("Enter side B: ");
+//     scanf("%lf", &B);
+
+//     C = sqrt(A*A + B*B);
+
+//     printf("Side C: %lf", C);
 
 
 //     return 0;
